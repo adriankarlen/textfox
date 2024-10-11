a49b01be19090bb47e875ff341a23676938774f4
adriankarlen <adrian.karlen@spp.se>
2024-10-11 16:24:17 +0200

    feat: add user.js

---
+++ user.js
@@ -0,0 +1,24 @@
+// userchrome.css usercontent.css activate
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Fill SVG Color
+user_pref("svg.context-properties.content.enabled", true);
+
+// CSS's `:has()` selector 
+user_pref("layout.css.has-selector.enabled", true);
+
+// Integrated calculator at urlbar
+user_pref("browser.urlbar.suggest.calculator", true);
+
+// Integrated unit convertor at urlbar
+user_pref("browser.urlbar.unitConversion.enabled", true);
+
+// Trim  URL
+user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.urlbar.trimURLs", true);
+
+// GTK rounded corners
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+
+// Who is bogus? (fixes Sidebery tab dragging on Linux)
+user_pref("widget.gtk.ignore-bogus-leave-notify", 1);
