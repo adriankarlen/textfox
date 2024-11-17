fd5d96d028c8157ef6b42266097460b67a199472
snaeil <52743746+snaeil@users.noreply.github.com>
2024-11-17 14:27:07 +0100

    feat: expose configuration options to home manager module (#58)

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -15,6 +15,91 @@ in {
       type = lib.types.str;
       description = "The profile to apply the textfox configuration to";
     };
+    config = lib.mkOption {
+      default = {};
+      type = lib.types.submodule {
+        options = {
+          background = lib.mkOption {
+            default = {};
+            type = lib.types.submodule {
+              options = {
+                color = lib.mkOption {
+                  type = lib.types.str;
+                  default = "var(--lwt-accent-color, -moz-dialog)";
+                  description = "Background color of all elements, tab colors derive from this";
+                };
+              };
+            };
+          };
+          displayHorizontalTabs = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+            description = "Enables horizontal tabs at the top";
+          };
+          font = lib.mkOption {
+            default = {};
+            type = lib.types.submodule {
+              options = {
+                family = lib.mkOption {
+                  type = lib.types.str;
+                  default = "\"SF Mono\", Consolas, monospace";
+                  description = "The font family to use";
+                };
+                size = lib.mkOption {
+                  type = lib.types.str;
+                  default = "14px";
+                  description = "The font size to use";
+                };
+                accent = lib.mkOption {
+                  type = lib.types.str;
+                  default = "var(--toolbarbutton-icon-fill)";
+                  description = "The accent color to use";
+                };
+              };
+            };
+          };
+          border = lib.mkOption {
+            default = {};
+            type = lib.types.submodule {
+              options = {
+                color = lib.mkOption {
+                  type = lib.types.str;
+                  default = "var(--arrowpanel-border-color, --toolbar-field-background-color)";
+                  description = "Border color when not hovered";
+                };
+                transition = lib.mkOption {
+                  type = lib.types.str;
+                  default = "0.2s ease";
+                  description = "Color transitions for borders";
+                };
+                width = lib.mkOption {
+                  type = lib.types.str;
+                  default = "2px";
+                  description = "Width of borders";
+                };
+                radius = lib.mkOption {
+                  type = lib.types.str;
+                  default = "0px";
+                  description = "Border radius used through out the config";
+                };
+              };
+            };
+          };
+          sidebery = lib.mkOption {
+            default = {};
+            type = lib.types.submodule {
+              options = {
+                margin = lib.mkOption {
+                  type = lib.types.str;
+                  default = "0.8rem";
+                  description = "Margin used between elements in sidebery";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -23,12 +108,28 @@ in {
       profiles."${cfg.profile}" = {
           extraConfig = builtins.readFile "${package}/user.js";
           extensions = [ config.nur.repos.rycee.firefox-addons.sidebery ];
-        };
-      };
-
-      home.file.".mozilla/firefox/${cfg.profile}/chrome" = {
-        source = "${package}/chrome";
-        recursive = true;
       };
     };
+
+    home.file.".mozilla/firefox/${cfg.profile}/chrome" = {
+        source = "${package}/chrome";
+        recursive = true;
+    };
+    home.file.".mozilla/firefox/${cfg.profile}/chrome/config.css" = {
+      text = lib.strings.concatStrings [
+        ":root {"
+        ( lib.strings.concatStrings [ " --tf-font-family: " cfg.config.font.family ";" ] )
+        ( lib.strings.concatStrings [ " --tf-font-size: " cfg.config.font.size ";" ] )
+        ( lib.strings.concatStrings [ " --tf-font-accent: " cfg.config.font.accent ";" ] )
+        ( lib.strings.concatStrings [ " --tf-background: " cfg.config.background.color ";" ] )
+        ( lib.strings.concatStrings [ " --tf-border-color: " cfg.config.border.color ";" ] )
+        ( lib.strings.concatStrings [ " --tf-border-transition: " cfg.config.border.transition ";" ] )
+        ( lib.strings.concatStrings [ " --tf-border-width: " cfg.config.border.width ";" ] )
+        ( lib.strings.concatStrings [ " --tf-border-radius: " cfg.config.border.radius ";" ] )
+        ( lib.strings.concatStrings [ " --tf-sidebery-margin: " cfg.config.sidebery.margin ";" ] )
+        ( lib.strings.concatStrings [ " --tf-display-horizontal-tabs: " ( if cfg.config.displayHorizontalTabs then "block" else "none" ) ";" ] )
+        " }"
+      ];
+    };
+  };
 }
