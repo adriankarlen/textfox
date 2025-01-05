2f60b7308ece56d5d8400d92bef6380195deb24b
Adrian Karlén <113891532+adriankarlen@users.noreply.github.com>
2025-01-06 00:53:37 +0100

    feat: add bash installation script (#83)

---
+++ tf-install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+read -e -p "Path to firefox profile: " path
+echo $path
+
+if [ -d "$path/chrome" ]; then
+  while true; do
+    read -p "This operation will copy the contents to your chrome dir, do you want to create a backup? (Y/N): " should_backup
+    case "$should_backup" in
+      [Yy]* ) cp -rf "$path/chrome" "$path/chrome_backup";
+        break
+        ;;
+      [Nn]* ) 
+        break
+        ;;
+      *) echo "Please answer Y or N."
+        ;;
+    esac
+  done
+else
+  mkdir "$path/chrome"
+fi
+
+cp -rf "chrome/"* "$path/chrome/"
+
+read -p "Do you want to install the user.js file? (Y/N)" install_js 
+
+case "$install_js" in
+  [Yy]* ) cp "user.js" "$path/user.js";
+    break
+    ;;
+  [Nn]* ) 
+    break
+    ;;
+  *) 
+    break
+    ;;
+esac
+
+echo "Installation completed, thank you for using textfox."
