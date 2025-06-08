8218565a439ad355aa5892e754f3412daa96b165
Sam Foreman <saforem2@gmail.com>
2025-06-07 19:38:40 -0500

    chore: Clean up `tf-install.sh`

--- tf-install.sh
+++ tf-install.sh
@@ -1,51 +1,89 @@
 #!/bin/bash
 
-read -e -p "Path to firefox profile: " path
-path="${path/#\~/$HOME}"  # Expand ~ to $HOME
-path="${path%/}"  # Remove trailing slash if exists
-path="$(echo "$path" | sed 's/\/\//\//g')"  # Normalize double slashes
-# remove single and double quotes
-path="$(echo "$path" | sed "s/[\"']//g")"
-if [ ! -d "$path" ]; then
-  echo "The specified path does not exist. Please check the path and try again."
-  exit 1
-fi
+print_logo() {
+  cat <<EOF
+   __            __  ____          
+  / /____  _  __/ /_/ __/___  _  __
+ / __/ _ \| |/_/ __/ /_/ __ \| |/_/
+/ /_/  __/>  </ /_/ __/ /_/ />  <  
+\__/\___/_/|_|\__/_/  \____/_/|_|  
+EOF
+}
 
-echo "Found profile at ${path}"
+clean_path() {
+  local fp="$1"
+  fp="${fp/#\~/$HOME}" # Expand ~ to $HOME
+  fp="${fp%/}"         # Remove trailing slash if exists
+  fp="${fp/\/\//\/}"   # Remove double slashes
+  fp="${fp//\'/}"      # Remove single quotes
+  fp="${fp//\"/}"      # Remove double quotes
+  echo "$fp"
+}
 
-if [ -d "$path/chrome" ]; then
-  read -p "This operation will copy the contents to your chrome dir, do you want to create a backup? (Y/N): " should_backup
-  bdir="${path}/chrome_backup-$(date +%Y%m%d_%H%M%S)"
-  case "$should_backup" in
-    [Yy]* )
-      [ ! -d "${bdir}" ] && mkdir "${bdir}"
-      echo "Creating backup of the existing chrome directory in ${bdir}";
-      cp -rf "$path/chrome" "$path/chrome_backup";
-      ;;
-    [Nn]* )
-      echo "No backup created, proceeding with the installation."
-      ;;
-    *) echo "Please answer Y or N."
-      ;;
-  esac
-else
-  mkdir "$path/chrome"
-fi
+backup_existing_profile() {
+  local fp="$1"
+  fp="$(clean_path "${fp}")"
+  if [[ -d "${fp}/chrome" ]]; then
+    echo "[!!] Backing up existing chrome directory..."
+    mv -v "${fp}/chrome" "${fp}/chrome-$(date +%Y%m%d_%H%M%S).bak"
+  fi
+}
 
-cp -rf "chrome/"* "$path/chrome/"
+copy_chrome() {
+  local fp="$1"
+  fp="$(clean_path "${fp}")"
+  if [[ -d "${fp}" ]]; then
+    echo "Copying textfox/chrome/ -> ${fp}/chrome/"
+    cp -r "chrome" "${fp}/chrome"
+  else
+    echo "The specified Firefox profile path does not exist: ${fp}"
+    return 1
+  fi
+}
 
-read -p "Do you want to install the user.js file? (Y/N): " install_js
+install_user_js() {
+  local fp="$1"
+  fp="$(clean_path "${fp}")"
 
-case "$install_js" in
-  [Yy]* ) cp "user.js" "$path/user.js";
-    ;;
-  [Nn]* )
-    echo "Skipping user.js installation."
+  # Optionally install user.js
+  read -rp "Do you want to install the user.js file? (Y/N): " install_js
+
+  case "$install_js" in
+  [Yy]*)
+    cp -v "user.js" "$fp/user.js"
     ;;
   *)
-    echo "Please answer Y or N."
+    echo "Skipping user.js installation."
     ;;
-esac
+  esac
+}
 
-echo "Installation completed, thank you for using textfox."
-exit 0
+tf_install() {
+  printf "\nInstalling textfox...\n"
+  local fp
+  if [[ "$#" -eq 1 ]]; then
+    fp="$(clean_path "$1")"
+  else
+    read -rp "Path to Firefox profile: " fp
+    fp="$(clean_path "${fp}")"
+  fi
+
+  ffpp="$(clean_path "${fp}")"
+  if [[ ! -d "${ffpp}" ]]; then
+    echo "[!!] Directory ${ffpp} does not exist"
+    while [[ ! -d "${ffpp}" ]]; do
+      read -rp "Path to Firefox profile: " fp
+      ffpp="$(clean_path "${fp}")"
+    done
+  fi
+
+  echo "Using Firefox Profile @ ${ffpp}"
+  backup_existing_profile "${ffpp}"
+  copy_chrome "${ffpp}"
+  install_user_js "${ffpp}"
+  printf "✓ Installation completed\n"
+}
+
+# clear
+print_logo
+tf_install "$@"
